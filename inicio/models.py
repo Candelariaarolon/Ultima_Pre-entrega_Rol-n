cac1ed55cc6a7e6e7a6b6a2178bd8ff667078efc
canderolon <candeerolonn@gmail.com>
2023-11-09 15:55:25 -0300

    actualizo render

--- inicio/models.py
+++ inicio/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Usuarios(models.Model):
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
-    edad= models.IntegerField
+    edad= models.IntegerField(default=0)
 
 # Create your models here.
